b8622f9ce42cae7234214f613c36b6ed7bb85d9a
Odeh <abuzaid163@hotmail.com>
2021-11-18 07:39:30 +0200

    ci: pages are being rendered as expected

--- topics/tests.py
+++ topics/tests.py
@@ -2,51 +2,51 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Snack
+from .models import Topic
 
 
-class SnackTests(TestCase):
+class TopicTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             username="Test_user", email="admin@admin.com", password="Pass123121"
         )
 
-        self.snack = Snack.objects.create(
-            title="test_snack",
-            purchaser=self.user,
-            description="test_snack description",
+        self.topic = Topic.objects.create(
+            title="test_topic",
+            student=self.user,
+            entries="entries..",
         )
 
     def test_str(self):
-        snack = Snack(title="test_snack")
-        self.assertEqual(str(snack), snack.title)
-
-    def test_snack_list_view(self):
-        response = self.client.get(reverse("snack_list"))
+        topic = Topic(title="test_topic")
+        self.assertEqual(str(topic), topic.title)
+    
+    def test_topic_list_view(self):
+        response = self.client.get(reverse("topic_list"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "test_snack")
-        self.assertTemplateUsed(response, "snack_list.html")
-
-    def test_snack_details(self):
-        response = self.client.get(reverse("snack_details", args=[1]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "title")
-        self.assertTemplateUsed(response, "snack_details.html")
-
-    def test_snack_create(self):
-        response = self.client.get(reverse("snack_create"))
+        self.assertContains(response, "test_topic")
+        self.assertTemplateUsed(response, "topics/topic_list.html")
+    
+    def test_topic_details(self):
+        response = self.client.get(reverse("topic_details", args=[1]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "title")
-        self.assertTemplateUsed(response, "snack_create.html")
-
-    def test_snack_update(self):
-        response = self.client.get(reverse("snack_update", args=[1]))
+        self.assertTemplateUsed(response, "topics/topic_detail.html")
+    
+    def test_topic_create(self):
+        response = self.client.get(reverse("topic_create"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "title")
-        self.assertTemplateUsed(response, "snack_update.html")
-
-    def test_snack_delete(self):
-        response = self.client.get(reverse("snack_delete", args=[1]))
+        self.assertTemplateUsed(response, "topics/topic_form.html")
+    
+    def test_topic_update(self):
+        response = self.client.get(reverse("topic_update", args=[1]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "title")
-        self.assertTemplateUsed(response, "snack_delete.html")
+        self.assertTemplateUsed(response, "topics/topic_form.html")
+    
+    def test_topic_delete(self):
+        response = self.client.get(reverse("topic_delete", args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "title")
+        self.assertTemplateUsed(response, "topics/topic_confirm_delete.html")
